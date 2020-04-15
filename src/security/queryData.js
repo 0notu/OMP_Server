0aef7314432d2f23f90b01317d7cee69e5119ca4
realnotU <notyoudev@gmail.com>
2020-04-14 21:14:45 -0400

    lmao idk

--- src/security/queryData.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto');
-
-module.exports.run = function(data, socket_id) {
-  console.log("under construction");
-  const secret = 'abcdefg';
-  const hash = crypto.createHmac('sha256', data);
-  hash.digest('hex');
-}
